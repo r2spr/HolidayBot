1fa7b0694b4621c88595bdb0916d18f1793e2e85
r2spr <105548769+r2spr@users.noreply.github.com>
2022-08-15 15:31:17 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -55,8 +55,9 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton(f"{START_BTN1_NAME}", f"{START_BTN1_URL}")
-    buttons.buildbutton(f"{START_BTN2_NAME}", f"{START_BTN2_URL}")
+    buttons.buildbutton("Owner", "https://t.me/r2spr")
+    buttons.buildbutton("Main Channel", "https://t.me/HombaleCinemas")
+    buttons.buildbutton("Mirror-Leech Group", "https://t.me/LeechGroupH")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
