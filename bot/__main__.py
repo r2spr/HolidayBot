5061323f95c6a6561192df9a104b73658bce1dc5
r2spr <105548769+r2spr@users.noreply.github.com>
2022-08-15 15:34:07 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,12 +61,12 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+This bot can leech-mirror/clone/drive all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user, Need to Join @HombaleCinemas mirror-leech bot Group ', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
